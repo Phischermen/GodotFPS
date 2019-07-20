2895a844e9831f6e70fb6a030e970fa71e9ad3b3
Phischermen <kevfischermen@gmail.com>
2019-07-20 13:36:41 -0700

    Implemented blob shadow

---
+++ Character/Scripts/blob_shadow_cast.gd
@@ -0,0 +1,44 @@
+extends RayCast
+
+onready var shadow:MeshInstance;
+
+export(float) var shadow_scale = 1.0;
+
+func _ready():
+	shadow = $BlobShadow;
+	
+	pass
+
+func _process(delta: float) -> void:
+	#ensure raycast is always facing downwards
+	transform.basis = get_parent().transform.basis.inverse();
+	if is_colliding():
+		shadow.visible = true;
+		
+		#put shadow slightly above ground to avoid z-fighting
+		shadow.translation = to_local(get_collision_point());
+		shadow.translation.y += 0.1;
+		
+		#scale shadow based on distance to floor
+		shadow.scale = Vector3.ONE * shadow_scale * (1 - shadow.translation.y / -cast_to.length());
+		
+		#get direction shadow is facing
+		var from:Vector3 = shadow.transform.basis.z;
+		
+		#get normal of ground
+		var to:Vector3 = get_collision_normal();
+		
+		#get the axis defined by these two vectors
+		var axis:Vector3 = from.cross(to).normalized();
+		
+		#get angle from -> to
+		var angle:float = from.angle_to(to);
+		
+		if abs(angle) > 0.01:
+			#angle is significant
+			var rotated_basis:Basis = shadow.transform.basis.rotated(axis, angle);
+			shadow.transform.basis = rotated_basis;
+			
+	else:
+		shadow.visible = false;
+	pass
