9bac11c9a88f6587aba3bf5ec212f0112f5eec36
Phischermen <kevfischermen@gmail.com>
2019-10-12 11:51:53 -0700

    Changed Debug Camera View into a resizable window.

--- Character/debug_view.gd
+++ Character/debug_view.gd
@@ -1,18 +1,20 @@
-extends ViewportContainer
+extends WindowDialog
 
 var on:bool = false;
 var viewport:Viewport;
 var camera:Camera;
 var playerCamera:Camera;
 export(NodePath) var playerCameraPath;
+export(NodePath) var debugCameraPath;
+export(NodePath) var ViewportPath;
 
 func _ready():
-	viewport = get_node("Viewport");
-	camera = get_node("Viewport/Camera");
+	viewport = get_node(ViewportPath);
+	camera = get_node(debugCameraPath);
 	playerCamera = get_node(playerCameraPath);
 	
 func _process(delta):
 	if Input.is_action_just_pressed("place_debug_camera"):
 		on = !on;
-		visible = on;
+		popup();
 		camera.global_transform = playerCamera.global_transform;
