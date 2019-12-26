eb0393040dd6980d84cfb6a0ec888f5fbb6b1823
Phischermen <kevfischermen@gmail.com>
2019-12-26 11:37:57 -0800

    Update pause.gd

--- pause.gd
+++ pause.gd
@@ -1,5 +1,8 @@
 extends Node
 
+onready var player = preload("res://Character/Scripts/Player.cs").get_Singleton();
+onready var save_data = player.call("Save");
+	
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		match(Input.get_mouse_mode()):
@@ -9,3 +12,7 @@ func _process(delta):
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene();
+	if Input.is_action_just_pressed("quick_save"):
+		save_data = player.call("Save");
+	if Input.is_action_just_pressed("quick_load"):
+		player.call("Load", save_data);
