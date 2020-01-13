0e1c0ba665fffd4cdcdbc9099beb8e429e92ceec
Phischermen <kevfischermen@gmail.com>
2020-01-13 14:25:35 -0800

    Update full_screen_switch.gd
    Can now switch to full screen, even when paused

--- full_screen_switch.gd
+++ full_screen_switch.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready() -> void:
+	pause_mode = Node.PAUSE_MODE_PROCESS;
+	
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen;
