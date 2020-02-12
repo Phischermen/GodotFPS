93334c0d0bf9ada96a24b5e2f47dd8ce2ae3a880
Phischermen <kevfischermen@gmail.com>
2020-02-11 19:22:30 -0800

    Added template script for interactable

---
+++ ScriptTemplates/Interactable.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class %CLASS% : Interactable
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    //Called when user interacts with object
+    public override void Interact()
+    {
+        
+    }
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
